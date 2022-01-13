88887d35c77a90cfe74389fe2fabb4963cb2ac88
Dev-hugo-hash <vitor.hugoss100@gmail.com>
2022-01-13 14:44:56 -0300

    Adicionadas as rotas aos demais html's

--- src/server.js
+++ src/server.js
@@ -4,11 +4,10 @@ server.use(express.static('/public'));
 
 const FilePath = __dirname + '/views/'
 
-server.get('/', (req,res) => {
-
-    res.sendFile(FilePath + "index.html");
-
-})
+server.get('/', (req,res) => res.sendFile(FilePath + "index.html"));
+server.get('/job', (req,res) => res.sendFile(FilePath + "job.html"));
+server.get('/job/edit', (req,res) => res.sendFile(FilePath + "job-edit.html"));
+server.get('/profile', (req,res) => res.sendFile(FilePath + "profile.html"));
 
 
 server.listen(3000, () => console.log('Server listening on port'));
