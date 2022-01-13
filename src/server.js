f0fba6cac1c5f0bf661f2c64790ac877d678e7ac
Dev-hugo-hash <vitor.hugoss100@gmail.com>
2022-01-13 14:44:12 -0300

    Criada pasta publicas com arquivos estaticos

--- src/server.js
+++ src/server.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const server = express();
+server.use(express.static('/public'));
 
+const FilePath = __dirname + '/views/'
 
+server.get('/', (req,res) => {
+
+    res.sendFile(FilePath + "index.html");
+
+})
 
 
 server.listen(3000, () => console.log('Server listening on port'));
