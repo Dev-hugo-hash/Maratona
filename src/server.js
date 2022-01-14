29ee7124661a0acc0d7f4ef19d07d86da84f8cd6
Dev-hugo-hash <vitor.hugoss100@gmail.com>
2022-01-13 23:47:48 -0300

    Iniciadas as alteracoes em ejs

--- src/server.js
+++ src/server.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const server = express();
+const routes = require('./routes')
+
 server.use(express.static('public'));
+server.set('view engine', 'ejs');
+server.use('/', routes);
 
-const filePath = __dirname + '/views/'
-
-server.get('/', (req,res) => res.sendFile(filePath + "index.html"));
-server.get('/job', (req,res) => res.sendFile(filePath + "job.html"));
-server.get('/job/edit', (req,res) => res.sendFile(filePath + "job-edit.html"));
-server.get('/profile', (req,res) => res.sendFile(filePath + "profile.html"));
 
 
 server.listen(3000, () => console.log('Server listening on port'));
