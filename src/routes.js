d39f77952340adbe8d7731ef5c9973390cb409fe
Dev-hugo-hash <vitor.hugoss100@gmail.com>
2022-01-14 21:11:16 -0300

    Criado objeto nas rotas para mostrar o meu perfil

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,21 @@
-const { Router } = require('express');
-const routes = Router()
+const express = require('express');
+const routes = express.Router()
+
+const profile = {
+    "name": 'Vitor Hugo ',
+    "avatar": 'https://avatars.githubusercontent.com/u/80496528?v=4',
+    "monthly-budget": "R$3.000",
+    "hours-per-day": 5,
+    "days-per-week": 5,
+    "vacation-per-year": 2
+}
+
 
 const filePath = __dirname + '/views/'
 
 routes.get('/', (req,res) => res.render(filePath + "index"));
 routes.get('/job', (req,res) => res.render(filePath + "job"));
 routes.get('/job/edit', (req,res) => res.render(filePath + "job-edit"));
-routes.get('/profile', (req,res) => res.render(filePath + "profile"));
+routes.get('/profile', (req,res) => res.render(filePath + "profile", {profile: profile}));
 
 module.exports = routes;
