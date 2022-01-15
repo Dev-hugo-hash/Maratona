cf6afa81e1e863208e08dff31057a849db19c1fd
Dev-hugo-hash <vitor.hugoss100@gmail.com>
2022-01-14 21:14:25 -0300

    Adicionado meu perfil na page principal

--- src/routes.js
+++ src/routes.js
@@ -13,7 +13,7 @@ const profile = {
 
 const filePath = __dirname + '/views/'
 
-routes.get('/', (req,res) => res.render(filePath + "index"));
+routes.get('/', (req,res) => res.render(filePath + "index", {profile: profile}));
 routes.get('/job', (req,res) => res.render(filePath + "job"));
 routes.get('/job/edit', (req,res) => res.render(filePath + "job-edit"));
 routes.get('/profile', (req,res) => res.render(filePath + "profile", {profile: profile}));
