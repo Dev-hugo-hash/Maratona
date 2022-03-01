4330ca1a1eb05ca99546a1f0fd3dcb9233c1fbda
Dev-hugo-hash <vitor.hugoss100@gmail.com>
2022-03-01 15:55:05 -0300

    Adicionado comentarios de auxilio

--- src/server.js
+++ src/server.js
@@ -2,10 +2,15 @@ const express = require('express');
 const server = express();
 const routes = require('./routes')
 
+//Pasta estática 
 server.use(express.static('public'));
+
+//EJS
 server.set('view engine', 'ejs');
+
+//routes
 server.use(routes);
 
 
-
+//Escutando a porta 
 server.listen(3000, () => console.log('Server listening on port'));
