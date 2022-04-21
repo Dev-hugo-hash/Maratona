8f4a41b0b3d057116a44b1e9180f3cab881203a9
Dev-hugo-hash <vitor.hugoss100@gmail.com>
2022-04-21 02:06:23 -0300

    Criado calculo de dias restantes

--- src/routes.js
+++ src/routes.js
@@ -7,7 +7,8 @@ const profile = {
     "monthly-budget": "R$3.000",
     "hours-per-day": 5,
     "days-per-week": 5,
-    "vacation-per-year": 2
+    "vacation-per-year": 2,
+    "value-hour": 75
 }
 
 const jobs = [
@@ -15,7 +16,7 @@ const jobs = [
         id:1,
         name: "Pizzaria Grego",
         "daily-hours": 2,
-        "total-hours": 60,   
+        "total-hours": 1,   
         created_at: Date.now()
     },
     {
@@ -27,9 +28,46 @@ const jobs = [
     }
 ]
 
+function remainingDays(job){
+
+    const remainingDays = (job["total-hours"] / job["daily-hours"]).toFixed()
+
+    const createdDate = new Date(job.created_at)//Dia que o job foi criado 
+    const dueDay = createdDate.getDate() + Number(remainingDays) //Dia da entrega 
+    const dueDate = createdDate.setDate(dueDay)//Data exata do vencimento
+
+    const timeDiffInMs = dueDate - Date.now() //Diferença de dias 
+        //Transformar Milli em dias
+    const dayInMs = 1000 * 60 * 60 * 24
+       const dayDiff = Math.floor(timeDiffInMs / dayInMs)//diferenca em dias arredonada para baixo
+
+    return dayDiff;
+}
+
 const filePath = __dirname + '/views/'
 
-routes.get('/', (req,res) => res.render(filePath + "index", {profile: profile, jobs }));
+routes.get('/', (req,res) =>{
+    //ajustes no job
+    //calculo de tempo restante
+    const updatedJobs = jobs.map((job) => {
+
+        const remaining = remainingDays(job)
+        const status = remaining <= 0 ? 'Done' : 'Progress'
+
+        return {
+            
+            ...job, //espalhamento
+            remaining,
+            status,
+            budget: profile["value-hour"] * job["total-hours"] //calculo de hora
+
+        }
+    })
+
+
+    return res.render(filePath + "index", {profile: profile, jobs: updatedJobs })
+
+});
 routes.get('/job', (req,res) => res.render(filePath + "job"));
 routes.post('/job', (req,res) => {
 
